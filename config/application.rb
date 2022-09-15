4579932aeda0727f68b22728891b3babeec3d434
mkellner43 <matthew.kellner@outlook.com>
2022-09-15 18:18:32 -0500

    fixing mailer issues

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,6 @@ module Facebook
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.action_mailer.default_url_options = { host: 'https://sleepy-mountain-79076.herokuapp.com' }
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
