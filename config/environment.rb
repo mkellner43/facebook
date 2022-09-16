15e721013f4439778f270eaeabf6f14eec695d77
mkellner43 <matthew.kellner@outlook.com>
2022-09-16 09:25:21 -0500

    fixing oauth issues

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-  :password => ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
+  :password => Rails.application.credentials.dig(:SENDGRID_API_KEY), # This is the secret sendgrid API key which was issued during API key creation
   :domain => 'sleepy-mountain-79076.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
