5661c2953a65816875c720492fa5e07e5c0d3f76
mkellner43 <matthew.kellner@outlook.com>
2022-09-19 19:17:52 -0500

    updated frontend

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -51,6 +51,6 @@ class PostsController < ApplicationController
   private
   
   def post_params
-    params.require(:post).permit(:id, :body)
+    params.require(:post).permit(:id, :body, :image)
   end
 end
